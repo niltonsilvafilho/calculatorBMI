c42bc7f03bf146318cebba30c5e3aa741ce2fa16
Nilton <niltonandfilho@gmail.com>
2023-12-27 09:00:37 -0300

    refactoring and componentizing my app

--- lib/main.dart
+++ lib/main.dart
@@ -30,30 +30,150 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-double _volume = 0.0;
-
 class _MyHomePageState extends State<MyHomePage> {
   TextEditingController controllerWeight = TextEditingController();
   TextEditingController controllerStature = TextEditingController();
   TextEditingController controllerAge = TextEditingController();
   String resultBMI = '0';
 
+  String dropdownValueStature = 'inch';
+  String dropdownValueWeight = 'lb';
+
+  List<String> listGender = [
+    'Man',
+    'Woman',
+  ];
+
+  Widget inputFields(
+      String titleLabel, String label, TextEditingController controller) {
+    return Expanded(
+        child: ListTile(
+      dense: true,
+      title: Text(
+        titleLabel,
+        style: const TextStyle(fontWeight: FontWeight.bold),
+      ),
+      subtitle: TextFormField(
+        decoration: InputDecoration(
+          labelText: label,
+          labelStyle: const TextStyle(fontSize: 14),
+          alignLabelWithHint: true,
+        ),
+        keyboardType: const TextInputType.numberWithOptions(),
+        controller: controller,
+        textAlign: TextAlign.center,
+      ),
+      isThreeLine: true,
+    ));
+  }
+
+  Widget customDropdownButton(List<String> listItems, titleLabel, label) {
+    return Expanded(
+      child: ListTile(
+          dense: true,
+          title: Text(
+            titleLabel,
+            style: const TextStyle(fontWeight: FontWeight.bold),
+          ),
+          subtitle: DropdownButtonFormField(
+            decoration: InputDecoration(
+              isDense: true,
+              labelText: label,
+              labelStyle: const TextStyle(fontSize: 14),
+              alignLabelWithHint: true,
+              contentPadding: const EdgeInsets.symmetric(vertical: 12.2),
+            ),
+            items: listItems.map((String items) {
+              return DropdownMenuItem(
+                value: items,
+                child: Text(items),
+              );
+            }).toList(),
+            onChanged: (String? newValue) {
+              setState(() {
+                dropdownValueStature = newValue!;
+              });
+            },
+          )),
+    );
+  }
+
+  Widget gaugeChart() {
+    return SizedBox(
+      width: 350,
+      height: 250,
+      child: SfRadialGauge(
+        axes: <RadialAxis>[
+          RadialAxis(
+              showLabels: false,
+              showAxisLine: false,
+              showTicks: false,
+              minimum: 0,
+              maximum: 50,
+              ranges: <GaugeRange>[
+                GaugeRange(
+                    startValue: 0,
+                    endValue: 18.5,
+                    color: const Color.fromARGB(255, 5, 41, 245),
+                    label: 'Under Weight',
+                    sizeUnit: GaugeSizeUnit.factor,
+                    labelStyle:
+                        const GaugeTextStyle(fontFamily: 'Times', fontSize: 20),
+                    startWidth: 0.65,
+                    endWidth: 0.65),
+                GaugeRange(
+                  startValue: 18.5,
+                  endValue: 24.99,
+                  color: const Color.fromARGB(255, 7, 248, 7),
+                  label: 'Healthy',
+                  labelStyle:
+                      const GaugeTextStyle(fontFamily: 'Times', fontSize: 15),
+                  startWidth: 0.65,
+                  endWidth: 0.65,
+                  sizeUnit: GaugeSizeUnit.factor,
+                ),
+                GaugeRange(
+                  startValue: 25.0,
+                  endValue: 50,
+                  color: const Color.fromARGB(255, 255, 1, 1),
+                  label: 'Overweight',
+                  labelStyle:
+                      const GaugeTextStyle(fontFamily: 'Times', fontSize: 20),
+                  sizeUnit: GaugeSizeUnit.factor,
+                  startWidth: 0.65,
+                  endWidth: 0.65,
+                ),
+              ],
+              pointers: <GaugePointer>[
+                NeedlePointer(
+                  value: double.parse(resultBMI),
+                  enableAnimation: true,
+                )
+              ])
+        ],
+      ),
+    );
+  }
+
+  Widget titles(String text) {
+    return Text(
+      text,
+      style: const TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+    );
+  }
+
+  void _calculate() {
+    double weight = double.parse(controllerWeight.text);
+    double stature = double.parse(controllerStature.text);
+    double imc = weight / (stature * stature);
+
+    setState(() {
+      resultBMI = imc.toStringAsFixed(2);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    String dropdownvalueStature = 'inch';
-    String dropdownvalueWeight = 'lb';
-
-    var itemsStature = [
-      'inch',
-      'foot',
-      'yard',
-      'mile',
-    ];
-
-    var itemsWeight = [
-      'lb',
-      'oz',
-    ];
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
@@ -62,181 +182,29 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Center(
         child: Padding(
-          padding: const EdgeInsets.all(16.0),
+          padding: const EdgeInsets.all(8.0),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              const Text(
-                'Calculate your BMI:',
-              ),
-              const SizedBox(height: 16),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  SizedBox(
-                    width: 100,
-                    child: TextFormField(
-                      keyboardType: const TextInputType.numberWithOptions(),
-                      controller: controllerAge,
-                      decoration: const InputDecoration(
-                          label: Center(
-                            child: Text("Age *"),
-                          ),
-                          border: UnderlineInputBorder()),
-                    ),
-                  ),
-                  SizedBox(
-                    width: 100,
-                    child: TextFormField(
-                      keyboardType: const TextInputType.numberWithOptions(),
-                      controller: controllerStature,
-                      decoration: const InputDecoration(
-                          label: Center(
-                            child: Text("Stature *"),
-                          ),
-                          border: UnderlineInputBorder()),
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
-                  SizedBox(
-                    width: 60,
-                    child: DropdownButton(
-                      itemHeight: 70,
-                      value: dropdownvalueStature,
-                      items: itemsStature.map((String items) {
-                        return DropdownMenuItem(
-                          value: items,
-                          child: Text(items),
-                        );
-                      }).toList(),
-                      onChanged: (String? newValue) {
-                        setState(() {
-                          dropdownvalueStature = newValue!;
-                        });
-                      },
-                    ),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 32),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      IconButton(
-                        icon: const Icon(Icons.woman),
-                        onPressed: () {},
-                      ),
-                      IconButton(
-                        icon: const Icon(Icons.man),
-                        onPressed: () {},
-                      ),
-                    ],
-                  ),
-                  SizedBox(
-                    width: 100,
-                    child: TextFormField(
-                      keyboardType: const TextInputType.numberWithOptions(),
-                      controller: controllerWeight,
-                      decoration: const InputDecoration(
-                          label: Center(
-                            child: Text("Weight *"),
-                          ),
-                          border: UnderlineInputBorder()),
-                    ),
-                  ),
-                  SizedBox(
-                    width: 60,
-                    child: DropdownButton(
-                      value: dropdownvalueWeight,
-                      items: itemsWeight.map((String items) {
-                        return DropdownMenuItem(
-                          value: items,
-                          child: Text(items),
-                        );
-                      }).toList(),
-                      onChanged: (String? newValue) {
-                        setState(() {
-                          dropdownvalueWeight = newValue!;
-                        });
-                      },
-                    ),
-                  ),
-                ],
-              ),
-              Container(
-                width: 500,
-                height: 400,
-                child: SfRadialGauge(
-                  axes: <RadialAxis>[
-                    RadialAxis(
-                        showLabels: false,
-                        showAxisLine: false,
-                        showTicks: false,
-                        minimum: 0,
-                        maximum: 50,
-                        ranges: <GaugeRange>[
-                          GaugeRange(
-                              startValue: 0,
-                              endValue: 18.4,
-                              color: Color.fromARGB(255, 5, 41, 245),
-                              label: 'Under Weight',
-                              sizeUnit: GaugeSizeUnit.factor,
-                              labelStyle: GaugeTextStyle(
-                                  fontFamily: 'Times', fontSize: 20),
-                              startWidth: 0.65,
-                              endWidth: 0.65),
-                          GaugeRange(
-                            startValue: 18.5,
-                            endValue: 24.99,
-                            color: Color.fromARGB(255, 7, 248, 7),
-                            label: 'Healthy',
-                            labelStyle: GaugeTextStyle(
-                                fontFamily: 'Times', fontSize: 20),
-                            startWidth: 0.65,
-                            endWidth: 0.65,
-                            sizeUnit: GaugeSizeUnit.factor,
-                          ),
-                          GaugeRange(
-                            startValue: 25.0,
-                            endValue: 50,
-                            color: Color.fromARGB(255, 255, 1, 1),
-                            label: 'Overweight',
-                            labelStyle: GaugeTextStyle(
-                                fontFamily: 'Times', fontSize: 20),
-                            sizeUnit: GaugeSizeUnit.factor,
-                            startWidth: 0.65,
-                            endWidth: 0.65,
-                          ),
-                        ],
-                        pointers: <GaugePointer>[
-                          NeedlePointer(
-                            value: double.parse(resultBMI),
-                            enableAnimation: true,
-                          )
-                        ])
-                  ],
-                ),
-              ),
-              Text(
-                resultBMI,
-                style:
-                    const TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-              ),
-              const SizedBox(height: 32),
+              Row(mainAxisAlignment: MainAxisAlignment.end, children: [
+                inputFields(
+                    "Weight *", "Enter To Weight (kg)", controllerWeight),
+                inputFields("Age *", "Enter to Age (years)", controllerAge),
+              ]),
+              Row(mainAxisAlignment: MainAxisAlignment.end, children: [
+                inputFields(
+                    "Stature *", "Enter To Stature (m)", controllerStature),
+                customDropdownButton(listGender, "Gender", "Select To Gender"),
+              ]),
+              const SizedBox(height: 12),
+              gaugeChart(),
+              titles(resultBMI),
+              const SizedBox(height: 12),
               SizedBox(
                 width: 200,
                 child: ElevatedButton(
                     onPressed: () {
-                      setState(() {
-                        num numAux = (num.parse(controllerWeight.text) /
-                            (num.parse(controllerStature.text) *
-                                num.parse(controllerStature.text)));
-
-                        resultBMI = numAux.toStringAsFixed(2);
-                      });
+                      _calculate();
                     },
                     child: const Text('Calculate')),
               )
@@ -252,7 +220,7 @@ class _MyHomePageState extends State<MyHomePage> {
         },
         tooltip: 'clean',
         child: const Text('Clean'),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
